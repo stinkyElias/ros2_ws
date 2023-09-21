80f4a9faa97d5d8036983f05f5c04c22518062dc
stinkyElias <elias.kalland@outlook.com>
2023-09-21 10:35:11 +0200

    Added an action client

---
+++ build/action_tutorials_interfaces/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/stinky/Documents/ros2_ws/src/action_tutorials_interfaces
+# Build directory: /home/stinky/Documents/ros2_ws/build/action_tutorials_interfaces
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("action_tutorials_interfaces__py")
